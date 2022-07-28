df3c0c974b5811e84a1e33c5143191c130170931
Marieta <marieta.sahakyan.8@gmail.com>
2022-07-28 19:50:36 +0400

    Bubble sort

---
+++ src/bubbleSort.java
@@ -0,0 +1,16 @@
+public class bubbleSort {
+    public static void BubbleSort(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < (array.length - i); j++) {
+                if (array[j - 1] > array[j]) {
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
+
+
